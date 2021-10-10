7c025fd4073246717176fed4b02b4a920d406cbd
lawlessbt <lawlessbt@gmail.com>
2021-10-10 11:11:01 +0530

    Update BaseTest.java

--- src/test/java/com/bt/testcases/BaseTest.java
+++ src/test/java/com/bt/testcases/BaseTest.java
@@ -6,11 +6,14 @@ import com.bt.base.Page;
 
 public class BaseTest {
 		
+	String a="hey ";
 	
 	
 	@AfterSuite
 	public void tearDown() throws InterruptedException {
 		Thread.sleep(4000);
 		Page.quit();
+		System.out.println(a+"how are?");
+
 	}
 }
